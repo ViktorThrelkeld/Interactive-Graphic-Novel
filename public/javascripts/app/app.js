a099d139e4210c55fbb28fc87f5cc7b70793da02
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2013-11-16 12:34:24 -0600

    add parallax script

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -6,17 +6,17 @@ var socket;
 
 function initialize(){
   $(document).foundation();
-  initializeSocketIO();
+  // initializeSocketIO();
 }
 
-function initializeSocketIO(){
-  var port = window.location.port ? window.location.port : '80';
-  var url = window.location.protocol + '//' + window.location.hostname + ':' + port + '/app';
+// function initializeSocketIO(){
+//   var port = window.location.port ? window.location.port : '80';
+//   var url = window.location.protocol + '//' + window.location.hostname + ':' + port + '/app';
 
-  socket = io.connect(url);
-  socket.on('connected', socketConnected);
-}
+//   socket = io.connect(url);
+//   socket.on('connected', socketConnected);
+// }
 
-function socketConnected(data){
-  console.log(data);
-}
+// function socketConnected(data){
+//   console.log(data);
+// }
