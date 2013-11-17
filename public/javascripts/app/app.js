82ff73c664a14cbf6ecb950779f6391622b5e619
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2013-11-16 18:53:46 -0600

    slides with image

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -2,20 +2,39 @@
 
 $(document).ready(initialize);
 
-var socket;
 
 function initialize(){
   $(document).foundation();
   $('#next').on('click', move);
-}
-
-parallax.add($("#bar"));
-
-function move(){
-  // alert('hey!');
-
-
-  parallax.bar.left(); //Bar slides in from the left
+  $('#back').on('click', back);
 
 }
+// debugger;
+
+          // .add($("#page1"));
+  // parallax.bar.hide(); //Bar is being a tad shy
+    parallax.add($("#robot"));
+            // .add($("#earth"));
+// parallax.earth.show();
+  // parallax.add($("#page1"));
+
+  function move(){
+    // alert('hey!');
+  parallax.add($("#earth"));
+
+  parallax.earth.hide(); //Bar is being a tad shy
+
+    parallax.robot.right(); //Bar slides in from the left
+    // $("#page1").hide();
+
+  }
+
+function back(){
+  // alert('yo');
+  // parallax.add($("robot"));
+  parallax.robot.hide(); //Bar is being a tad shy
+
+  parallax.earth.left();
+}
+//         // parallax.index.show();
 
