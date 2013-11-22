1dd84a30e50785cca97e330dfa4b3ad54c35d81e
ViktorThrelkeld <Viktorthrelkeld@gmail.com>
2013-11-22 09:21:36 -0600

    add front page

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require('require-dir')('./models');
 
 // route definitions
 var home = require('./routes/home');
+var comic = require('./routes/comic');
 
 var app = express();
 var RedisStore = require('connect-redis')(express);
@@ -16,6 +17,7 @@ require('./config').initialize(app, RedisStore);
 
 // routes
 app.get('/', home.index);
+app.get('/comic', comic.index);
 
 // start server & socket.io
 var common = require('./sockets/common');
---
+++ routes/comic.js
@@ -0,0 +1,6 @@
+
+
+
+exports.index = function(req, res){
+  res.render('comic/index', {title: 'comic'});
+};
